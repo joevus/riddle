336dd36fc641bff1af1b8687d51b4df1c3019dce
Joe Hoskisson <joehoskisson@gmail.com>
2022-11-25 16:53:45 -0700

    show number of correct answers in winner board

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -21,6 +21,14 @@ class Competition < ApplicationRecord
     guess == self.answer_key.key
   end
 
+  def correct_answer_number
+    # alternative way to count number of correct answers
+    # there is also the correct_answer_count field on competition.
+    # but this is more up to date while testing
+    # will probably just want to have one way at some point
+    Winner.where(:competition_id => self.id).count
+  end
+
   def process_guess(guess)
     update(:correct_answer_count => correct_answer_count + 1) if is_guess_correct(guess)
     is_guess_correct(guess)
